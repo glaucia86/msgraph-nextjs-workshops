695817cd18baefb20089fb001ef12d67c7e3ebdb
glaucia86 <glaucia_lemos86@hotmail.com>
2022-10-11 13:17:01 -0300

    developing the 'Header' component

--- demo/reminder-app/components/Header.js
+++ demo/reminder-app/components/Header.js
@@ -6,16 +6,36 @@
  */
 
 import Link from 'next/link';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Header() {
+  const { data: session } = useSession();
+
+  const handleSignin = (e) => {
+    e.preventDefault();
+    signIn();
+  };
+
+  const handleSignout = (e) => {
+    e.preventDefault();
+    signOut();
+  };
+
   return (
     <div className='header'>
       <Link href='/'>
         <a className='logo'>NextJs + MS Graph</a>
       </Link>
-      <a href='#' className='btn-signin'>
-        Sign in
-      </a>
+      {session && (
+        <a href='#' onClick={handleSignout} className='btn-signin'>
+          Sign out
+        </a>
+      )}
+      {!session && (
+        <a href='#' onClick={handleSignin} className='btn-signin'>
+          Sign in
+        </a>
+      )}
     </div>
   );
 }
